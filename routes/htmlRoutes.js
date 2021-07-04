8bb38794da1b5f8e3e91f4cc973baa0984b28f5a
LawrieDrew <laurenn.drew@gmail.com>
2021-07-04 17:17:50 -0400

    fixed html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,6 +2,10 @@ const path = require('path');
 
 module.exports = (app) => {
 
+app.get("/index", (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, '../public/assets/index.html'));
 
@@ -11,4 +15,4 @@ app.get("notes", function (req, res) {
     res.sendFile(path.join(__dirname, '../public/assets/notes.html'));
 });
 
-}
+};
