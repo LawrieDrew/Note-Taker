1f7a05020e8e7778dc5a0a0fe6a4144f350c06b9
LawrieDrew <laurenn.drew@gmail.com>
2021-07-04 17:07:47 -0400

    created proper read me, fixed api routes

--- README.md
+++ README.md
@@ -1,3 +1,51 @@
 # Note-Taker
 
-An App Clearly Intended to Make Me Cry
+ 
+# Description 
+
+A note taking application that be used to write, save, or delete notes utilizing Express.
+
+# Table of Contents
+
+* [Installation](#installation)
+
+* [Usage](#Usage)
+
+* [Deployed Link to Heroku](#Link)
+
+* [License](#License)
+
+# Installation
+
+The following dependencies must be installed to run application: fs, express, db/db.json/path, and uuid.
+# Usage
+
+Used by me in a futile attempt to pass this course.
+
+# Deployed Link to Heroku
+
+
+
+# License 
+
+MIT License
+
+Copyright (c) 2021 Lauren Drew
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,46 +1,37 @@
-//require fs, path, require('uuid/v1'), and express
-// create a variable called router and set it to express.router()
-
 const fs = require('fs');
-const path = require('path');
-const express = require('express');
-const router = require('express').Router();
+const db = require('../db/db.json');
+
 const uuid = require('uuid');
 
 module.exports = (app) => {
 
-    //get the notes
-app.get("/api/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "../db/db.json"))
-});
+    app.get('api/notes', (req, res) => res.json(db));
 
-    //post the notes & allocate id
+    app.post('api/notes', (req, res) => {
+        console.log('Duly noted!');
+        const newNote = req.body;
+        newNote.id = uuid();
+        console.log(newNote.id);
+        db.push(newNote)
+        console.log(
+            `'title': '${newNote.title}',
+           'text': '${newNote.text}',
+           'id': '${newNote.id}',
+           posted to db.json`
+        );
+        fs.writeFile('../db/db.json', JSON.stringify(db), (err) => {
+            if (err) throw err;
+            console.log('New Note Up!')
+        });
+        res.json(newNote);
+    })};
 
-app.post("/api/notes", (req, res) => {
-    console.log('Duly Noted!');
-    const newNote = req.body;
-    newNote.id = uuid;
-    db.push(newNote.id);
-//  console.log(
-//     "title":"Test Title 1",
-//     "text":"Test text",
-//     "id": 1)
-
-
-//if there's an error, throw 500 message
-
-if (err) throw err;
-console.log('500');
-});
-
-    //delete note
-app.delete("/api/notes/:id", (req, res) => {
-    const notes = JSON.parse(fs.readFileSync('.db/db.json'));
-    const deleteNote = notes.filter((removeNote) => removeNote.id ! == req.params.id);
-    fs.writeFileSync('./db/db.json', JSON.stringify(deleteNote));
-    res.json(deleteNote);
-})
-
- 
-
-}
+    app.delete('/api/notes/:id'), (req, res) => {
+        console.log(req.params.id);
+        const newDB = db.filter((deleteNote => deleteNote.id != req.params.id);
+        fs.writeFile('../db/db.json', JSON.stringify(newDB), (err) => {
+            if(err) throw err;
+        });
+        console.log('Note successfully chucked!');
+        res.json('word!');
+        };
